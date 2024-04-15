1ff22662afa552fd4e0b703418fac767cf61c505
mihail-gosman <mihailc.gosman@gmail.com>
2024-04-15 19:16:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import src.leibniz
 
 
+def seek():
+
+
+
 def main():
     while True:
         command = input("piSeeker> ")
 
+        command = command.split()
+
         if command == "exit":
             exit()
+
+        elif command[0] == "seek":
+            seek()
 
         else:
             print("Error: command not found!")
